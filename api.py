767bc41e07be60209af0f8521ece5f7a26787a60
Vinho0oNguyen <78035648+Vinho0oNguyen@users.noreply.github.com>
2022-09-07 23:09:37 +0700

    Update api.py

--- api.py
+++ api.py
@@ -2,6 +2,6 @@ from bottle import route, run
 import json
 @route('/mail/a')
 def hello():
-    return json.dumps({"message": "hello v2"})
+    return json.dumps({"message": "ptithcm"})
 
 run(host='0.0.0.0', port=5000)
